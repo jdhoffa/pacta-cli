ac5b584b272339cfcf2d957e817db3f5ca8df920
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-08-16 18:05:04 +0000

    feat: Add dockerized hello world (#1)
    * add reqwest and tokio deps
    
    * ignore image tarball
    
    * add main.rs that pulls hello-world from ghcr
    
    * add Cargo lockfile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Url;
+use std::process::Command;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt; // Import AsyncWriteExt trait
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Define the image URL
+    let image_url = Url::parse("https://ghcr.io/jonashackt/hello-world:latest")?;
+
+    // Download the image tarball
+    let response = reqwest::get(image_url).await?;
+    let image_data = response.bytes().await?;
+
+    // Create or open the file
+    let mut image_file = File::create("image.tar").await?;
+
+    image_file.write_all(&image_data).await?;
+
+    // Pull the Docker image from the downloaded tarball
+    Command::new("docker")
+        .args(&["load", "-i", "image.tar"])
+        .status()?;
+
+    // Run a Docker container to display ASCII art
+    Command::new("docker")
+        .args(&["run", "--rm", "hello-world"])
+        .status()?;
+
+    Ok(())
 }
